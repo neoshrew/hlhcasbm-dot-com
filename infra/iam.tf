9afbf9229bbd2adf2fd94043baad0451d4689270
Andy Gelnar <apgelnar@gmail.com>
2018-09-16 17:18:34 +0100

    cloudfront needs list access on the bucket

--- infra/iam.tf
+++ infra/iam.tf
@@ -8,14 +8,14 @@ data "aws_iam_policy_document" "site_s3_access" {
       identifiers = ["${aws_cloudfront_origin_access_identity.site.iam_arn}"]
     }
   }
-  #
-  # statement {
-  #   actions   = ["s3:ListBucket"]
-  #   resources = ["${aws_s3_bucket.site.arn}"]
-  #
-  #   principals {
-  #     type        = "AWS"
-  #     identifiers = ["${aws_cloudfront_origin_access_identity.site.iam_arn}"]
-  #   }
-  # }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = ["${aws_s3_bucket.site.arn}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.site.iam_arn}"]
+    }
+  }
 }
