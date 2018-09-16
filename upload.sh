4c0229c96859c9855e5bcd030f83cc13787facae
Andy Gelnar <apgelnar@gmail.com>
2018-09-16 16:28:06 +0100

    Set nuptual date when publishing
    this is a hack, but meh

--- upload.sh
+++ upload.sh
@@ -7,6 +7,13 @@ function finish {
 }
 trap finish EXIT
 
+now=$(date +%Y-%m-%dT%H:%M:%S%z)
+printf "Setting nuptuals to now - %s\n" "${now}"
+# Note to future generations - this isn't ideal
+regex="^(\s*const marriedAt = new Date\(').*?('\);)"
+repl="\1${now}\2"
+sed -Ei "s/${regex}/${repl}/" site/index.html
+
 cd infra
 printf "Grabbing TF output\n"
 terraform output -json > $TMPFILE
@@ -16,7 +23,7 @@ distro_id=$(cat $TMPFILE | jq -r '.cloudfront_id.value')
 bucket_name=$(cat $TMPFILE | jq -r '.bucket.value')
 
 printf "Syncing to bucket %s\n" "${bucket_name}"
-aws s3 sync ./site s3://apgelnar-hlhcasbm-site/
+aws s3 sync ./site s3://"${bucket_name}/"
 
 # printf "creating invalidation in distro id %s\n" "${distro_id}"
 # aws cloudfront create-invalidation \
